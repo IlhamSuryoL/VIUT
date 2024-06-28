c97a83d3cd7bc27cb0574e32de986a3b59c6377e
putu eka mulyana <putuekamulyana97@gmail.com>
2024-06-28 18:25:21 +0800

    ✨ feat: Register User

--- Viut/App.js
+++ Viut/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
       <StatusBar style="auto" />
-      <LoginScreen />
+      <RegisterScreen />
     </View>
   );
 }
--- Viut/src/screens/RegisterScreen.js
+++ Viut/src/screens/RegisterScreen.js
@@ -5,27 +5,32 @@ import Button from '../components/Button'
 import ButtonAuthLink from '../components/ButtonAuthLink'
 import formatTextStyle from '../styles/formatTextStyle'
 import TextInput from '../components/TextInput'
-import { Entypo } from '@expo/vector-icons';
+import { Entypo, FontAwesome6 } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 
 const COLOR_ICON = "#B9B8B8"
-const LoginScreen = ({ navigation }) => {
+const RegisterScreen = ({ navigation }) => {
   return (
     <View style={{ backgroundColor: colors.black, flex: 1, padding: 20, justifyContent: 'center' }}>
-      <Text style={[formatTextStyle({ color: '#fff', fontSize: 24, fontWeight: '700' }), { textAlign: 'center', marginBottom: 60 }]}>Login</Text>
+      <Text style={[formatTextStyle({ color: '#fff', fontSize: 24, fontWeight: '700' }), { textAlign: 'center', marginBottom: 60 }]}>Register</Text>
+      <TextInput label={"Nama"}
+        placeholder={"Masukkan nama anda"}
+        iconLeft={<FontAwesome6 name="user" size={24} color={COLOR_ICON} />}
+      />
       <TextInput label={"Email"}
         placeholder={"Masukkan Email anda"}
         iconLeft={<Entypo name="email" size={24} color={COLOR_ICON} />}
+        containerStyle={{ marginTop: 20 }}
       />
       <TextInput label={"password"}
         placeholder={"Masukkan password anda"}
         iconLeft={<MaterialIcons name="lock-outline" size={24} color={COLOR_ICON} />}
         containerStyle={{ marginTop: 20 }}
       />
-      <Button text="Login" style={{ width: 239, alignSelf: 'center', marginVertical: 40 }} onPress={() => navigation.navigate('HomeScreen')} />
-      <ButtonAuthLink text="Belum memiliki akun?" cta="Daftar" />
+      <Button text="Daftar" style={{ width: 239, alignSelf: 'center', marginVertical: 40 }} onPress={() => navigation.navigate('HomeScreen')} />
+      <ButtonAuthLink text="Sudah memiliki akun?" cta="Login" />
     </View>
   )
 }
 
-export default LoginScreen
+export default RegisterScreen
