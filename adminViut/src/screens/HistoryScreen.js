4da968491540d70d36d5b89dd1a57ad285be2152
putu eka mulyana <putuekamulyana97@gmail.com>
2024-07-17 22:33:16 +0800

    ✨ feat: implement history

--- adminViut/src/screens/HistoryScreen.js
+++ adminViut/src/screens/HistoryScreen.js
@@ -1,22 +1,51 @@
 import { View, Text, FlatList } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import WrapperView from '../components/WrapperView'
 import TextPrimary from '../components/TextPrimary'
 import ProductImage from '../components/ProductImage'
 import TextSmall from '../components/TextSmall'
+import FullLoading from '../components/FullLoading'
+import { collection, getDocs, orderBy, query } from 'firebase/firestore'
+import { FIRE_DB } from '../../firebaseConfig'
 
 const HistoryScreen = ({ navigation }) => {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const getProducts = async () => {
+    setLoading(true)
+    const tampData = []
+    const querySnapshot = await getDocs(query(collection(FIRE_DB, "products"), orderBy("update_at", "desc")));
+    querySnapshot.forEach((doc) => {
+      const product = doc.data()
+      if (product?.users) {
+        product.users.forEach((user) => {
+          tampData.push({
+            id: doc.id,
+            ...product,
+            ...user
+          })
+        })
+      }
+    });
+    setData(tampData)
+    setLoading(false)
+  }
+  useEffect(() => {
+    getProducts()
+  }, [])
+
   const _renderHistoryItem = (product) => {
     return (
       <View style={{ borderWidth: 1, borderColor: 'grey', borderRadius: 10, paddingHorizontal: 20, paddingVertical: 10 }}>
         <TextPrimary style={{ color: '#000', textAlign: 'flex-start' }} text={product.createAt} />
         <View style={{ flexDirection: 'row', borderTopWidth: 1, borderTopColor: 'grey', paddingTop: 18 }}>
-          <ProductImage />
+          <ProductImage uri={product.product_image_url} />
           <View style={{ marginLeft: 19 }}>
-            <TextPrimary style={{ color: '#000', textAlign: "flex-start" }} text={product.productName} />
-            <TextSmall text={`Peserta ${product.participant}`} />
-            <TextSmall text={`Skor VIAT-Q : ${product.scoreQuestion}`} />
-            <TextSmall text={`Skor NPS : ${product.scoreNps}`} />
+            <TextPrimary style={{ color: '#000', textAlign: "flex-start" }} text={product.product_name} />
+            <TextSmall text={product?.name || ""} />
+            <TextSmall text={`Skor VIAT-Q : ${product.viat_score}`} />
+            <TextSmall text={`Skor NPS : ${product.nps_score}`} />
           </View>
         </View>
       </View>
@@ -25,17 +54,13 @@ const HistoryScreen = ({ navigation }) => {
   return (
     <WrapperView>
       <FlatList
-        data={[1, 2, 4]}
-        renderItem={() => _renderHistoryItem({
-          createAt: "14 April 2024",
-          productName: "kacamata",
-          participant: 1,
-          scoreQuestion: 2,
-          scoreNps: 4
-        })}
+        keyExtractor={(i, index) => index.toString()}
+        data={data}
+        renderItem={({ item }) => _renderHistoryItem(item)}
         ItemSeparatorComponent={<View style={{ height: 23 }} />}
         ListHeaderComponent={<TextPrimary style={{ color: '#000', marginBottom: 23 }} text={"Riwayat Penilaian Produk Anda"} />}
       />
+      {loading && <FullLoading />}
     </WrapperView>
   )
 }
