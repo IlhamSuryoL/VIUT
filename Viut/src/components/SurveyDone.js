5833b7ecc499460ac6a603bf6db86b1177771e53
putu eka mulyana <putuekamulyana97@gmail.com>
2024-07-18 21:59:46 +0800

    🐞 fix: use username

--- Viut/src/components/SurveyDone.js
+++ Viut/src/components/SurveyDone.js
@@ -3,13 +3,16 @@ import React from 'react'
 import TextSmall from './TextSmall'
 import TextPrimary from './TextPrimary'
 import colors from '../styles/colors'
+import { userAtom } from '../store'
+import { useAtomValue } from 'jotai'
 
 const SurveyDone = ({ labelTop, labelBottom, totalScore }) => {
+  const user = useAtomValue(userAtom)
   return (
     <View>
       <TextSmall text={labelTop} style={{ textAlign: 'center' }} />
       <View style={{ paddingHorizontal: 28, paddingVertical: 14, backgroundColor: colors.blue, borderRadius: 10, marginVertical: 80 }}>
-        <TextPrimary text={"Nama user"} />
+        <TextPrimary text={user?.name || ""} />
         <TextPrimary text={`Score : ${totalScore}`} />
       </View>
       <TextSmall text={labelBottom} style={{ textAlign: 'center' }} />
